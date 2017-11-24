1c419f14b76746261145059f4bddbcf0ef961dde
Piotr Wesolowski <piotr.wesolowski@euvic.pl>
2017-11-25 00:28:20 +0100

    Editing light and player position

--- ChilloutHackathonApp/Assets/OUR_Scripts/MenuManager.cs
+++ ChilloutHackathonApp/Assets/OUR_Scripts/MenuManager.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class MenuManager : MonoBehaviour
 {
     public GameObject menuLight;
+    public Transform playPosition;
+    public GameObject player;
+    public List<GameObject> lights;
 
     private float secondsToLightUp = 5;
 
     private IEnumerator Start()
     {
         yield return new WaitForSecondsRealtime(secondsToLightUp);
+        menuLight.SetActive(true);
+        yield return new WaitForSecondsRealtime(10);
+        menuLight.SetActive(false);
+        player.transform.position = playPosition.position;
+        yield return new WaitForSecondsRealtime(0.5f);
+        foreach(GameObject light in lights)
+        {
+            light.SetActive(true);
+        }
+
     }
 }
