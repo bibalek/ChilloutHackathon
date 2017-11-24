4e9fb896eb89ba17dd8123ca3200a324cd911de9
Piotr Wesolowski <piotr.wesolowski@euvic.pl>
2017-11-25 00:13:54 +0100

    Create lights

---
+++ ChilloutHackathonApp/Assets/OUR_Scripts/MenuManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject menuLight;
+
+    private float secondsToLightUp = 5;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSecondsRealtime(secondsToLightUp);
+    }
+}
