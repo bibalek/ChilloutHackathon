840f80cb771dc14931d4c538b16500ecbfd47dfc
Piotr Wesolowski <piotr.wesolowski@euvic.pl>
2017-11-25 06:15:21 +0100

    Create Start button, animations, better interior, new start section, sounds, particle system

--- ChilloutHackathonApp/Assets/OUR_Scripts/MenuManager.cs
+++ ChilloutHackathonApp/Assets/OUR_Scripts/MenuManager.cs
@@ -7,22 +7,42 @@ public class MenuManager : MonoBehaviour
     public GameObject menuLight;
     public Transform playPosition;
     public GameObject player;
+    public GameObject table;
+    public GameObject menu;
     public List<GameObject> lights;
 
     private float secondsToLightUp = 5;
+    public MenuManager instance;
 
     private IEnumerator Start()
     {
+        instance = this;
         yield return new WaitForSecondsRealtime(secondsToLightUp);
         menuLight.SetActive(true);
-        yield return new WaitForSecondsRealtime(10);
+        player.GetComponent<Animator>().SetTrigger("MainMenu");
+        yield return new WaitForSecondsRealtime(1.5f);
+        menu.SetActive(true);
+    }
+
+    public void PrepareAndStart()
+    {
+        menu.SetActive(false);
+        StartCoroutine(PrepareLights());
+    }
+
+    private IEnumerator PrepareLights()
+    {
         menuLight.SetActive(false);
+        table.SetActive(true);
         player.transform.position = playPosition.position;
+
         yield return new WaitForSecondsRealtime(0.5f);
-        foreach(GameObject light in lights)
+        foreach (GameObject light in lights)
         {
             light.SetActive(true);
+            yield return new WaitForSecondsRealtime(0.15f);
         }
-
+        menuLight.SetActive(true);
     }
+
 }
